a9cad6d68ec0964aa24dd39e7b7f82316f5617d8
Aryan Bamane <aryanbamane2@gmail.com>
2024-03-21 16:39:31 +0530

    Added Outfit Font

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+
 // import ReactDOM from "react-dom/client";
 import {
   Route,
@@ -15,16 +16,18 @@ import Home from "./pages/Home.jsx";
 import Layout from "./components/Layout.jsx";
 import UserContextProvider from "./context/UserContextProvider.jsx";
 import Services from "./pages/Services.jsx";
+import NotFound from "./components/NotFound.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
+    <Route path="/" element={<Layout />} errorElement={<NotFound />}>
       <Route path="" element={<Home />} />
       <Route path="projects" element={<Projects />} />
       <Route path="blog" element={<Blogs />} />
       <Route path="contact-me" element={<Contact />} />
       <Route path="story" element={<Other />} />
       <Route path="services" element={<Services />} />
+      <Route path="404" element={<NotFound />} />
     </Route>
   )
 );
@@ -35,7 +38,6 @@ const App = () => {
       <UserContextProvider>
         <RouterProvider router={router} />
       </UserContextProvider>
-      
     </>
   );
 };
